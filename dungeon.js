c6b8b853637da5896d44ca18c24b873527fbc4af
Jacob Speakman <speakman610@gmail.com>
2024-02-15 14:22:24 -0700

    Enforce movement rules

--- dungeon.js
+++ dungeon.js
@@ -1,13 +1,20 @@
 const dungeon = document.querySelector('#dungeon')
-const gameData = {points: 0, knights: 0, bishops: 0, rooks: 0, numItems: 0, floor: 1}
+const gameData = { points: 0, knights: 0, bishops: 0, rooks: 0, numItems: 0, floor: 1 }
 updateInfo()
 
+// Rooms and things
 const EMPTY = 0
 const START = 1
 const END = 2
 const CHEST = 3
 const MINION = 4
 const BOSS = 5
+
+// Movement calculation
+const N_S_step = 8
+const W_E_step = 1
+const NW_SE_step = 9
+const SW_NE_step = 7
 
 /* 
   Room Types:
@@ -42,19 +49,19 @@ function createFloor(floor, start) {
   while (rooms.length < 64) {
     if (rooms.length === start) {
       rooms.push(START) // this will be the room the player starts in
-    } else if (rooms.length === start + 1
-      || rooms.length === start - 1
-      || rooms.length === start + 7
-      || rooms.length === start - 7
-      || rooms.length === start + 8
-      || rooms.length === start - 8
-      || rooms.length === start + 9
-      || rooms.length === start - 9) {
+    } else if (rooms.length === start + W_E_step
+      || rooms.length === start - W_E_step
+      || rooms.length === start + SW_NE_step
+      || rooms.length === start - SW_NE_step
+      || rooms.length === start + N_S_step
+      || rooms.length === start - N_S_step
+      || rooms.length === start + NW_SE_step
+      || rooms.length === start - NW_SE_step) {
       // if we are on one of the spaces surrounding the start space
       rooms.push(EMPTY)
     } else {
       const rand = Math.floor(Math.random() * 99) // get a number between 0 and 99
-      if (rand <= 69 || numThings === 2*floor) { // 70% chance on floor 1
+      if (rand <= 69 || numThings === 2 * floor) { // 70% chance on floor 1
         rooms.push(EMPTY)
       } else if (rand <= 74) { // 5% chance on floor 1
         rooms.push(CHEST)
@@ -75,7 +82,7 @@ function drawFloor(floor, rooms) {
   rooms.forEach((room, i) => {
     const square = document.createElement('div')
     square.classList.add('square')
-    switch(room) {
+    switch (room) {
       case START:
         square.innerHTML = player
         break
@@ -101,14 +108,14 @@ function drawFloor(floor, rooms) {
         square.innerHTML = chest
         break
     }
-    
+
     if (room === START) square.firstChild?.setAttribute('draggable', true) // only do this if this is the player
     square.setAttribute('square-id', i)
-    const row = Math.floor( (63 - i) / 8) + 1
+    const row = Math.floor((63 - i) / 8) + 1
     if (row % 2 === 0) {
-        square.classList.add(i % 2 === 0 ? 'white-square' : 'black-square')
+      square.classList.add(i % 2 === 0 ? 'white-square' : 'black-square')
     } else {
-        square.classList.add(i % 2 === 0 ? 'black-square' : 'white-square')
+      square.classList.add(i % 2 === 0 ? 'black-square' : 'white-square')
     }
     dungeon.append(square)
   })
@@ -135,10 +142,31 @@ function drawFloor(floor, rooms) {
     const start = document.querySelector(`[square-id="${startId}"]`)
     const isOccupied = target.firstChild
 
-    if (!start.firstChild && !start.firstChild.classList.contains('player')) return
+    if (startId === targetId || (!start.firstChild && !start.firstChild.classList.contains('player'))) return
+
+    // Move validation
+    if (!validateDiagonal(targetId, startId, true) && !validateAdjacent(targetId, startId, true)) {
+      // Here we need to check all of the powerups they have
+      const knight = gameData.knights && validateKnight(targetId, startId)
+      const bishop = gameData.bishops && validateDiagonal(targetId, startId)
+      const rook = gameData.rooks && validateAdjacent(targetId, startId)
+
+      if (!knight && !bishop && !rook) {
+        return
+      } else if (knight) {
+        gameData.knights--
+        updateInfo()
+      } else if (bishop) {
+        gameData.bishops--
+        updateInfo()
+      } else if (rook) {
+        gameData.rooks--
+        updateInfo()
+      }
+    }
 
     if (isOccupied) {
-      switch(target.firstChild.id) {
+      switch (target.firstChild.id) {
         case 'chest':
           gameData.knights++
           gameData.bishops++
@@ -169,7 +197,7 @@ function drawFloor(floor, rooms) {
             gameData.floor++
             break
           } else {
-            alert('Not enough points to move on to next floor')
+            alert(`Need ${gameData.floor - gameData.points} more points`)
             return
           }
       }
@@ -196,9 +224,9 @@ function drawFloor(floor, rooms) {
 
   const allSquares = document.querySelectorAll('#dungeon .square')
   allSquares.forEach(square => {
-      square.addEventListener('dragstart', dragStart)
-      square.addEventListener('dragover', dragOver)
-      square.addEventListener('drop', dragDrop)
+    square.addEventListener('dragstart', dragStart)
+    square.addEventListener('dragover', dragOver)
+    square.addEventListener('drop', dragDrop)
   })
 }
 
@@ -215,7 +243,7 @@ function addTask() {
   newTask.textContent = taskName
 
   // Add event listener to mark todo as completed
-  newTask.addEventListener('click', function() {
+  newTask.addEventListener('click', function () {
     this.remove()
     gameData.points++
     updateInfo()
@@ -228,6 +256,151 @@ function addTask() {
 function updateInfo() {
   const info = document.getElementById('info')
   info.innerText = `Floor: ${gameData.floor}\nPoints: ${gameData.points}\nKnights: ${gameData.knights}\nBishops: ${gameData.bishops}\nRooks: ${gameData.rooks}`
+}
+
+function validateDiagonal(targetId, startId, limit = false) {
+  let currentId = startId
+
+  const startIsEdge = isBoardEdge(startId)
+
+  while (currentId >= 0 && currentId <= 63) {
+    if (targetId > startId) {
+      if ((targetId - currentId) % NW_SE_step === 0 && !startIsEdge.edge.includes('E')) {
+        currentId += NW_SE_step
+      } else if ((targetId - currentId) % SW_NE_step === 0 && !startIsEdge.edge.includes('W')) {
+        currentId += SW_NE_step
+      } else {
+        return false
+      }
+    } else {
+      if ((currentId - targetId) % NW_SE_step === 0 && !startIsEdge.edge.includes('W')) {
+        currentId -= NW_SE_step
+      } else if ((currentId - targetId) % SW_NE_step === 0 && !startIsEdge.edge.includes('E')) {
+        currentId -= SW_NE_step
+      } else {
+        return false
+      }
+    }
+
+    if (limit || isBoardEdge(currentId).bool) {
+      return targetId === currentId
+    }
+
+    if (targetId === currentId) return true
+  }
+
+  return false
+}
+
+function validateAdjacent(targetId, startId, limit = false) {
+  let currentId = startId
+
+  const startIsEdge = isBoardEdge(startId)
+  const sameEdge = startIsEdge.bool ? isBoardEdge(targetId).edge.some(value => startIsEdge.edge.includes(value)) : false
+
+  while (currentId >= 0 && currentId <= 63) {
+    if (targetId > startId) {
+      if ((targetId - currentId) % N_S_step === 0) {
+        currentId += N_S_step
+      } else if ((targetId - currentId) < N_S_step && !startIsEdge.edge.includes('E')) {
+        currentId += W_E_step
+      } else {
+        return false
+      }
+    } else {
+      if ((currentId - targetId) % N_S_step === 0) {
+        currentId -= N_S_step
+      } else if ((currentId - targetId) < N_S_step && !startIsEdge.edge.includes('W')) {
+        currentId -= W_E_step
+      } else {
+        return false
+      }
+    }
+
+    if (limit || (isBoardEdge(currentId).bool && !sameEdge)) {
+      return targetId === currentId
+    }
+
+    if (targetId === currentId) return true
+  }
+
+  return false
+}
+
+function validateKnight(targetId, startId) {
+  let steps = [6, 10, 15, 17]
+  const sign = targetId > startId ? 1 : -1
+
+  const knightOnEdge = isBoardEdge(startId)
+  const knightOnInnerEdge = {
+    bool: false,
+    edge: ''
+  }
+
+  if (startId >= 1 && startId <= 57 && (startId - 1) % 8 === 0) {
+    knightOnInnerEdge.bool = true
+    knightOnInnerEdge.edge = 'IW'
+  }
+
+  if (startId >= 6 && startId <= 62 && (startId + 2) % 8 === 0) {
+    knightOnInnerEdge.bool = true
+    knightOnInnerEdge.edge = 'IE'
+  }
+
+  if (knightOnEdge.bool || knightOnInnerEdge.bool) {
+    if (sign < 0) { // -
+      if (knightOnEdge.edge.includes('W')) steps = [6, 15]
+      if (knightOnEdge.edge.includes('E')) steps = [10, 17]
+      if (knightOnInnerEdge.edge === 'IW') steps = [6, 15, 17]
+      if (knightOnInnerEdge.edge === 'IE') steps = [10, 15, 17]
+    } else { // +
+      if (knightOnEdge.edge.includes('W')) steps = [10, 17]
+      if (knightOnEdge.edge.includes('E')) steps = [6, 15]
+      if (knightOnInnerEdge.edge === 'IW') steps = [10, 15, 17]
+      if (knightOnInnerEdge.edge === 'IE') steps = [6, 15, 17]
+    }
+  }
+
+  for (let step of steps) {
+    if (targetId === startId + (sign * step)) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function isBoardEdge(id) {
+  const edge = {
+    bool: false,
+    edge: []
+  }
+
+  // TOP EDGE
+  if (id < 8) {
+    edge.bool = true
+    edge.edge.push('N')
+  }
+
+  // BOTTOM EDGE
+  if (id > 55) {
+    edge.bool = true
+    edge.edge.push('S')
+  }
+
+  // LEFT EDGE
+  if (id % 8 === 0) {
+    edge.bool = true
+    edge.edge.push('W')
+  }
+
+  // RIGHT EDGE
+  if ((id + 1) % 8 === 0) {
+    edge.bool = true
+    edge.edge.push('E')
+  }
+
+  return edge
 }
 
 createFloor(gameData.floor, 0)
